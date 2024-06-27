9262c2bb929a32267c8a2408013d58211162c092
Maria Alejandra Rodriguez Salas <alejamaria94@hotmail.com>
2024-06-27 18:37:49 -0500

    Arregla desplegables del menu

---
+++ src/app/private/pages/products/update-product/update-product.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+// import { ProductsService } from '../../../services/products.service';
+
+
+@Component({
+  selector: 'app-update-product',
+  template: '<h1> </h1>',
+  styles: [ '' ]
+})
+export class UpdateProductComponent {
+
+  product: any;
+  id: any
+
+  constructor(
+    // private productService: ProductsService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.product = new FormGroup({
+      name: new FormControl( '', [] ),
+      description: new FormControl( '', [] ),
+      price: new FormControl( 0, [] ),
+      urlimagen: new FormControl( '', []),
+      quantity: new FormControl( 0, [] ),
+    });
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe( ( data:any ) => {
+      this.id = data.id;
+
+      // this.productService.getProductById(this.id).subscribe( ( data: any) => {
+      //   console.log( data );
+
+      //   const productoEncontrado = data.data;
+
+      //   this.product.setValue({
+      //     name: productoEncontrado.name,
+      //     description: productoEncontrado.description,
+      //     price: productoEncontrado.price,
+      //     urlimagen: productoEncontrado.urlimagen,
+      //     quantity: productoEncontrado.quantity
+      //   });
+      // });
+    });
+  }
+
+  onSumbit() {
+    console.log( this.product.value )
+    if(this.product.valid){
+      console.log(this.product.value);
+      this.product.patchproduct(this.id, this.product.value).subscribe( (data: any) => {
+        console.log (data);
+      });
+      
+    }
+  }
+
+
+}
